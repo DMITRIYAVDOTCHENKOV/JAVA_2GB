9d5cb59afa2d5a8dbc26fd4b4457fe8547598dfe
DMITRIYAVDOTCHENKOV <dimka.a2010@yandex.ru>
2022-02-11 21:43:01 +0300

    Lesson

--- src/ru/avdotchenkov/lesson1/Homework2/Cat.java
+++ src/ru/avdotchenkov/lesson1/Homework2/Cat.java
@@ -1,6 +1,17 @@
 package ru.avdotchenkov.lesson1.Homework2;
 
 public class Cat implements Participan{
+    private final String name;
+    private final int run;
+    private final int jump;
+
+    public Cat(String name, int run, int jump) {
+        this.name = name;
+        this.run = run;
+        this.jump = jump;
+    }
+
+
     @Override
     public void run(int distance) {
 
@@ -9,5 +20,14 @@ public class Cat implements Participan{
     @Override
     public void jump(int height) {
 
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", runDistance=" + run +
+                ", jumpHeight=" + jump +
+                '}';
     }
 }
